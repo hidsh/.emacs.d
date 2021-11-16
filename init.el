f7c6fd19cd011cfbc9be964c5c5c17588b830c32
hidsh <775213+hidsh@users.noreply.github.com>
2021-11-17 04:01:37 +0900

    mod flymake-eslint for js

--- init.el
+++ init.el
@@ -3091,8 +3091,18 @@ according to `my-org-todo-publish-cemetery-accept-titles'."
   )
 
 ;; ----------------------------------------------------------------------
+(use-package web-mode
+  :mode (("html" . web-mode))
+  :config
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+  )
+
+;; ----------------------------------------------------------------------
 (use-package flymake-eslint
-  :hook ((js-mode . flymake-eslint-enable))
+  :hook ((js-mode  . flymake-eslint-enable)
+         (web-mode . flymake-eslint-enable))
   )
 
 ;; ----------------------------------------------------------------------
