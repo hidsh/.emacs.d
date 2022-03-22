15b862d0e36b8aa0b1f576571ecb9861f29c19cd
hidsh <775213+hidsh@users.noreply.github.com>
2022-03-22 09:53:39 +0900

    add packages

--- custom.el
+++ custom.el
@@ -6,14 +6,12 @@
  '(ansi-color-faces-vector
    [default bold shadow italic underline bold bold-italic bold])
  '(custom-safe-themes
-   (quote
-    ("9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "8042fe67484449b0315a39732a08002bb6d8e9196ed5c1e8a60d9c05107bfb28" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "78496062ff095da640c6bb59711973c7c66f392e3ac0127e611221d541850de2" default)))
+   '("9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "8042fe67484449b0315a39732a08002bb6d8e9196ed5c1e8a60d9c05107bfb28" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "78496062ff095da640c6bb59711973c7c66f392e3ac0127e611221d541850de2" default))
  '(hl-sexp-background-color "#1c1f26")
  '(org-startup-folded nil)
  '(org-startup-indented t)
  '(package-selected-packages
-   (quote
-    (bm auto-rename-tag expand-region web-mode swap-buffers projectile ah vterm flycheck-posframe ccls lsp-mode smartparens company-quickhelp company yasnippet org-tree-slide default-text-scale migemo dimmer zone-rainbow super-save hide-mode-line doom-themes doom-modeline all-the-icons all-the-icons-ivy git-gutter symbol-overlay slime clojure-mode wgrep-ag tabbar common-header-mode-line use-package evil-lion org-bullets arduino-mode beacon anzu evil-tutor-ja evil-collection evil google-translate guide-key-tip rainbow-delimiters fringe-helper git-gutter-fringe counsel-etags counsel-gtags neotree gist hiwin rainbow-mode telephone-line scratch-log markdown-mode dashboard ag ido-yes-or-no atom-one-dark-theme))))
+   '(cmake-mode fontawesome lsp-ui json-mode web-beautify nodejs-repl bm auto-rename-tag expand-region web-mode swap-buffers projectile ah vterm flycheck-posframe ccls lsp-mode smartparens company-quickhelp company yasnippet org-tree-slide default-text-scale migemo dimmer zone-rainbow super-save hide-mode-line doom-themes doom-modeline all-the-icons all-the-icons-ivy git-gutter symbol-overlay slime clojure-mode wgrep-ag tabbar common-header-mode-line use-package evil-lion org-bullets arduino-mode beacon anzu evil-tutor-ja evil-collection evil google-translate guide-key-tip rainbow-delimiters fringe-helper git-gutter-fringe counsel-etags counsel-gtags neotree gist hiwin rainbow-mode telephone-line scratch-log markdown-mode dashboard ag ido-yes-or-no atom-one-dark-theme)))
 
 (put 'narrow-to-region 'disabled nil)
 (custom-set-faces
---
+++ packages/cmake-mode-20210104.1831/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "ddc6be2c196fab3eedf5e243cb0fb387a52a4271")
