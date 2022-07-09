7ab17e9aebd4c251c5e0f29ead99f540a4c714b3
hidsh <775213+hidsh@users.noreply.github.com>
2022-07-09 09:08:21 +0900

    add/mod packages

---
+++ elpa/blackout-20200404.1550/blackout-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from blackout.el  -*- no-byte-compile: t -*-
+(define-package "blackout" "20200404.1550" "Better mode lighter overriding" '((emacs "26")) :commit "c221fa2c8a204b4aff2e09c606f59be58b960b97" :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :keywords '("extensions") :url "https://github.com/raxod502/blackout")
---
+++ elpa/leaf-20211226.1633/leaf-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from leaf.el  -*- no-byte-compile: t -*-
+(define-package "leaf" "20211226.1633" "Simplify your init.el configuration, extended use-package" '((emacs "24.1")) :commit "9eb18e8c9c375aa0158fbd06ea906bfbf54408fe" :authors '(("Naoya Yamashita" . "conao3@gmail.com")) :maintainer '("Naoya Yamashita" . "conao3@gmail.com") :keywords '("lisp" "settings") :url "https://github.com/conao3/leaf.el")
