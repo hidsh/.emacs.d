3096f8c93c91570b5200fb09024b066db9c149f8
hidsh <775213+hidsh@users.noreply.github.com>
2022-07-09 09:04:12 +0900

    add cmake-ide

--- init.el
+++ init.el
@@ -3862,49 +3862,12 @@ Thx to https://qiita.com/duloxetine/items/0adf103804b29090738a"
   )
 
 ;; ----------------------------------------------------------------------
-;; (use-package lsp-ui
-;;   ;; :disabled
-;;   :commands lsp-ui-mode
-;;   :after lsp-mode
-;;   :custom
-;;   ;; lsp-ui-doc
-;;   (lsp-ui-doc-enable t)
-;;   (lsp-ui-doc-header t)
-;;   (lsp-ui-doc-include-signature t)
-;;   (lsp-ui-doc-position 'top)
-;;   (lsp-ui-doc-max-width  60)
-;;   (lsp-ui-doc-max-height 20)
-;;   (lsp-ui-doc-use-childframe t)
-;;   (lsp-ui-doc-use-webkit nil)
+(use-package cmake-ide
+  :ensure t
+  :config
+  (cmake-ide-setup)
+  )
 
-;;   ;; lsp-ui-flycheck
-;;   (lsp-ui-flycheck-enable nil)
-
-;;   ;; lsp-ui-sideline
-;;   (lsp-ui-sideline-enable t)
-;;   (lsp-ui-sideline-ignore-duplicate t)
-;;   (lsp-ui-sideline-show-symbol t)
-;;   (lsp-ui-sideline-show-hover t)
-;;   (lsp-ui-sideline-show-diagnostics t)
-;;   (lsp-ui-sideline-show-code-actions t)
-
-;;   ;; lsp-ui-imenu
-;;   (lsp-ui-imenu-enable nil)
-;;   (lsp-ui-imenu-kind-position 'top)
-
-;;   ;; lsp-ui-peek
-;;   (lsp-ui-peek-enable t)
-;;   (lsp-ui-peek-always-show t)
-;;   (lsp-ui-peek-peek-height 30)
-;;   (lsp-ui-peek-list-width 30)
-;;   (lsp-ui-peek-fontify 'always)
-;;   :hook
-;;   (lsp-mode . lsp-ui-mode)
-;;   :bind
-;;   (("C-l s"   . lsp-ui-sideline-mode)
-;;    ("C-l C-d" . lsp-ui-peek-find-definitions)
-;;    ("C-l C-r" . lsp-ui-peek-find-references))
-;;   )
 ;; ----------------------------------------------------------------------
 (use-package ccls
   :ensure t
