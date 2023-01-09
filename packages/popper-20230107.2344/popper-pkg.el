e385cc4468f1b1438b51a9cb9616ca020f2dd867
hidsh <775213+hidsh@users.noreply.github.com>
2023-01-09 14:57:39 +0900

    add packages

---
+++ packages/popper-20230107.2344/popper-pkg.el
@@ -0,0 +1,12 @@
+(define-package "popper" "20230107.2344" "Summon and dismiss buffers as popups"
+  '((emacs "26.1"))
+  :commit "50568b8af2ee4c43d4a5b55241fac7ef11e2f9db" :authors
+  '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainer
+  '("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/karthink/popper")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
