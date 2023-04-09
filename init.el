248ea0fb6ed1f44315d0a4d5f9538e1aa74773fc
hidsh <775213+hidsh@users.noreply.github.com>
2023-04-10 04:48:49 +0900

    mod auto scroll for *Messages* buffer

--- init.el
+++ init.el
@@ -4764,11 +4764,28 @@ For example, `consult-recent-file' try to embed its preview into popper window i
 
 ;; ----------------------------------------------------------------------
 ;; auto scroll for *message* buffer
-(defun my-adv-message-auto-scroll (&rest _)
-  (save-current-buffer
-    (set-buffer "*Messages*")
-    (goto-char (point-max))))
-(advice-add 'message :after #'my-adv-message-auto-scroll)
+;; https://stackoverflow.com/questions/4682033/in-emacs-can-i-set-up-the-messages-buffer-so-that-it-tails
+(defun modi/messages-auto-tail (&rest _)
+  "Make *Messages* buffer auto-scroll to the end after each message."
+  (let* ((buf-name "*Messages*")
+         ;; Create *Messages* buffer if it does not exist
+         (buf (get-buffer-create buf-name)))
+    ;; Activate this advice only if the point is _not_ in the *Messages* buffer
+    ;; to begin with. This condition is required; otherwise you will not be
+    ;; able to use `isearch' and other stuff within the *Messages* buffer as
+    ;; the point will keep moving to the end of buffer :P
+    (when (not (string= buf-name (buffer-name)))
+      ;; Go to the end of buffer in all *Messages* buffer windows that are
+      ;; *live* (`get-buffer-window-list' returns a list of only live windows).
+      (dolist (win (get-buffer-window-list buf-name nil :all-frames))
+        (with-selected-window win
+          (goto-char (point-max))))
+      ;; Go to the end of the *Messages* buffer even if it is not in one of
+      ;; the live windows.
+      (with-current-buffer buf
+        (goto-char (point-max))))))
+
+(advice-add 'message :after #'modi/messages-auto-tail)
 
 ;; ----------------------------------------------------------------------
 ;; customize setting
